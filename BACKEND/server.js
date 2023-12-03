d15835326db5f44b56285bdf78c6c15fe1c1ccdc
Bandaranayaka <3lakshana1124@gmail.com>
2023-12-04 04:06:17 +0530

    done connection

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -13,3 +13,20 @@ app.use(bodyParser.json());
 
 
 const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useCreateIndex: true,
+    useNewUrlParser : true,
+    useUnifiedTopologyL : true,
+    useFindAndModify : false
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Mongodb Connection success!");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number : ${PORT}`)
+})
+
